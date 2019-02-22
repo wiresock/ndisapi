d819dd497dd81c2012bf44826da9661f6f58c569
Vadim Smirnov <vadim@ntkernel.com>
2019-02-23 00:19:49 +0200

    Version updated to 3.2.18.1

--- include/Common.h
+++ include/Common.h
@@ -20,9 +20,9 @@
 #include <WinIoctl.h>   // Compiling Win32 Applications Or DLL's
 #endif // _WINDOWS
 
-#define NDISRD_VERSION			0x02113000
+#define NDISRD_VERSION			0x02123000
 #define NDISRD_MAJOR_VERSION	0x0003
-#define NDISRD_MINOR_VERSION	0x0211
+#define NDISRD_MINOR_VERSION	0x0212
 
 // Common strings set
 #define DRIVER_NAME_A "NDISRD"
